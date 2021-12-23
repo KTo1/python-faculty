d737ee67305eeb121e993ec0ef37792e12886b02
KTo <kto@lovetou.ru>
2021-12-23 16:08:52 +0700

    Первая задача

--- algorithms-structures/lesson-04/practice/task_1.py
+++ algorithms-structures/lesson-04/practice/task_1.py
@@ -12,6 +12,10 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+"""
+
+"""
+
 from timeit import timeit
 
 
@@ -21,3 +25,21 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if nums[i] % 2 == 0]
+
+
+def func_3(nums):
+    pass
+    # return [i for i in range(0, len(nums), 2)]
+
+
+if __name__ == '__main__':
+    nums = list(range(10000))
+    print(timeit('func_1(nums)', number=1000, globals=globals()))
+    print(timeit('func_2(nums)', number=1000, globals=globals()))
+    print(timeit('func_3(nums)', number=1000, globals=globals()))
+
+    print(func_1(nums) == func_2(nums) == func_3(nums))
