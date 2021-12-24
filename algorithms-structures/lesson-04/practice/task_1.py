3dcff0835ba31508a383222658cf51ae87402860
KTo <kto@lovetou.ru>
2021-12-24 09:38:46 +0700

    Вот теперь точно все)

--- algorithms-structures/lesson-04/practice/task_1.py
+++ algorithms-structures/lesson-04/practice/task_1.py
@@ -19,6 +19,9 @@
 Из всего что мне пришло в голову быстрее всего оказались lc. Пока быстрее решения мне найти не удалось.
 По материалам из интернета я делаю вывод, что lc быстрее цикла for потому, что lc тратит меньше времени на 
 добавление, поскольку не вызывает метод append. 
+
+Так же я придумал еще решение, зависящее от прикладной задачи, это генератор) если нам нужно будет перебирать 
+этот массив по порядку, то можно создать генератор  
 """
 
 from timeit import timeit
@@ -37,11 +40,16 @@ def func_2(nums):
     return [i for i in range(len(nums)) if nums[i] % 2 == 0]
 
 
+def func_3(nums):
+    return (i for i in range(len(nums)) if nums[i] % 2 == 0)
+
+
 if __name__ == '__main__':
     nums = [randint(0, 10000) for i in range(10000)]
 
     print('Вариант 1:', timeit('func_1(nums)', number=1000, globals=globals()))
     print('Вариант 2:', timeit('func_2(nums)', number=1000, globals=globals()))
+    print('Вариант 3:', timeit('func_3(nums)', number=1000, globals=globals()))
 
-    print(f'Результат:{" " if func_1(nums) == func_2(nums) else "не "}корректен')
+    print(f'Результат:{" " if func_1(nums) == func_2(nums) == list(func_3(nums)) else "не "}корректен')
 
