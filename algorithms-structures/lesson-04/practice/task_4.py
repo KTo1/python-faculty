b45c2205c47cf7c890ddee01eb04f0930c842dea
KTo <kto@lovetou.ru>
2021-12-24 12:43:21 +0700

    домой

--- algorithms-structures/lesson-04/practice/task_4.py
+++ algorithms-structures/lesson-04/practice/task_4.py
@@ -12,8 +12,14 @@
 
 """
 ВЫВОДЫ:
-оба предлагаемых решения имеют сложность О(n^2) и даже просто переписав на линейную сложность мы получаем 
-выигрыш в производительности.
+Оба предлагаемых решения имеют сложность О(n^2). Даже при увеличении количества элементов с 7 до 14,
+и переписав на линейную сложность мы получаем выигрыш в производительности.
+
+Вариант 3, это вариант 2, с проверкой уже постчитанных результатов, будет хорошо работать если в массиве много 
+повторяющихся значений
+
+Предлагаемый вариант решения 4, самый быстрый, по причинам описанным выше
+Вариант 5 одной строкой, можно ускорить, если принт убрать)   
  
 """
 
@@ -21,7 +27,8 @@ from timeit import timeit
 
 
 array = [1, 3, 1, 3, 4, 5, 1]
-# array = [1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1]
+array = [1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1]
+array = [1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1, 1, 3, 1, 3, 4, 5, 1]
 
 
 def func_1():
@@ -49,41 +56,45 @@ def func_2():
 
 
 def func_3():
-    m, num = 0, 0
+    new_array = []
     dct = {}
-    for i in array:
-        dct.setdefault(i, 0)
-        dct[i] += 1
+    for el in array:
+        if el in dct:
+            continue
+        count2 = array.count(el)
+        new_array.append(count2)
+        dct[el] = el
 
-    for k, v in dct.items():
-        if v > m:
-            m, num = v, k
-
-    return f'Чаще всего встречается число {num}, ' \
-           f'оно появилось в массиве {m} раз(а)'
-
+    max_2 = max(new_array)
+    elem = array[new_array.index(max_2)]
+    return f'Чаще всего встречается число {elem}, ' \
+           f'оно появилось в массиве {max_2} раз(а)'
 
 def func_4():
     m, num = 0, 0
     dct = {}
     for i in array:
         dct.setdefault(i, 0)
-        dct[i] += 1
-
-    for k, v in dct.items():
-        if v > m:
-            m, num = v, k
+        cur_dict_i = dct[i] + 1
+        dct[i] = cur_dict_i
+        if cur_dict_i > m:
+            m, num = cur_dict_i, i
 
     return f'Чаще всего встречается число {num}, ' \
            f'оно появилось в массиве {m} раз(а)'
 
 
-if __name__ == '__main__':
-    print(timeit('func_1()', number=100000, globals=globals()))
-    print(timeit('func_2()', number=100000, globals=globals()))
-    print(timeit('func_3()', number=100000, globals=globals()))
-    print(timeit('func_4()', number=100000, globals=globals()))
+def func_5():
+    dct = {array.count(i): i for i in array}
+    return f'Чаще всего встречается число {dct[max(dct)]}, ' \
+           f'оно появилось в массиве {max(dct)} раз(а)'
 
-    print(func_1())
-    print(func_2())
-    print(func_3())
+
+if __name__ == '__main__':
+    print(f'Вариант 1: {timeit("func_1()", number=100000, globals=globals())}')
+    print(f'Вариант 2: {timeit("func_2()", number=100000, globals=globals())}')
+    print(f'Вариант 3: {timeit("func_3()", number=100000, globals=globals())}')
+    print(f'Вариант 4: {timeit("func_4()", number=100000, globals=globals())}')
+    print(f'Вариант 5: {timeit("func_5()", number=100000, globals=globals())}')
+
+    print(f'Результат:{" " if func_1() == func_2() == func_3() == func_4()  == func_5() else "не "}корректен')
