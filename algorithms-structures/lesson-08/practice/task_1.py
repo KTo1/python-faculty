b110151db84e6eaf3aea01f6f9f966277a0842f6
KTo <kto@lovetou.ru>
2022-01-20 23:06:34 +0700

    задание 2

--- algorithms-structures/lesson-08/practice/task_1.py
+++ algorithms-structures/lesson-08/practice/task_1.py
@@ -1,6 +1,5 @@
 """
 Задание 1.
-
 Реализуйте кодирование строки по алгоритму Хаффмана.
 У вас два пути:
 1) тема идет тяжело? тогда вы можете, опираясь на примеры с урока,
@@ -8,81 +7,74 @@
 Разрешается и приветствуется изменение имен переменных,
 выбор других коллекций, различные изменения
 и оптимизации.
-
 2) тема понятна? постарайтесь сделать свою реализацию.
 Вы можете реализовать задачу, например,
 через ООП или предложить иной подход к решению.
 """
 
 """
-https://habr.com/ru/post/438512/
+Ну, вроде получилось, получается, что чтобы потом этот код разобрать, нужно вместе с текстом передавать частоты и в 
+приемнике строить такое же дерево.
 """
 
 from collections import Counter, deque
 
 
 class BinaryTree:
-    def __init__(self, left=None, right=None, value=0):
+    def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
-        self.value = value
 
 
 class HaffmanTree:
+    def __init__(self):
+        self._code = []
+
     def encode(self, string):
-        # строим дерево
-        tree = self.build(string)
-
-        # расставляем 0, 1
-        self._walk(tree)
-
-        # собираем коды
+        self._walk(self.build(string))
 
     def build(self, string):
-        return self._haffman_tree(self._initial(string))
+        def __build(seq):
+            if len(seq) == 1:
+                return seq[0]
 
-    def _walk(self, node):
+            el1, el2 = seq.popleft(), seq.popleft()
+            weight = el1[1] + el2[1]
+            new_el = BinaryTree(left=el1[0], right=el2[0])
+
+            if len(seq) <= 0:
+                seq.insert(0, new_el)
+                return seq[0]
+            else:
+                for i in range(len(seq)):
+                    if seq[i][1] >= weight:
+                        seq.insert(i, (new_el, weight))
+                        break
+                    if i == len(seq) - 1:
+                        seq.append((new_el, weight))
+                __build(seq)
+            return seq[0]
+
+        return __build(self._initial(string))
+
+    def _walk(self, node, left=None):
+        if left is not None:
+            self._code.append('0') if left else self._code.append('1')
         if isinstance(node, str):
-            print(node)
+            # print(node, self._code)
+            print(''.join(self._code), end='')
+            self._code.pop()
             return
-        if node.left is not None:
-            print(0)
-            self._walk(node.left)
-        if node.right is not None:
-            print(1)
-            self._walk(node.right)
+        self._walk(node.left, True)
+        self._walk(node.right, False)
+        if len(self._code) > 0:
+            self._code.pop()
 
     def _initial(self, string):
-        sequence = deque(sorted(dict(Counter(string)).items(), key=lambda i: i[1]))
-        for i in range(len(sequence)):
-            sequence[i] = (BinaryTree(sequence[i][0], value=sequence[i][1]), sequence[i][1])
-
-        return sequence
-
-    def _haffman_tree(self, seq):
-        if len(seq) == 1:
-            return seq[0]
-
-        el1, el2 = seq.popleft(), seq.popleft()
-        weight = el1[1] + el2[1]
-        new_el = BinaryTree(left=el1[0], right=el2[0], value=weight)
-        if len(seq) <= 0:
-            seq.insert(0, new_el)
-            return seq[0]
-        else:
-            for i in range(len(seq)):
-                if seq[i][1] >= weight:
-                    seq.insert(i, (new_el, weight))
-                    break
-                if i == len(seq)-1:
-                    seq.append((new_el, weight))
-            self._haffman_tree(seq)
-        return seq[0]
+        return deque(sorted(dict(Counter(string)).items(), key=lambda i: i[1]))
 
 
 if __name__ == '__main__':
     s = 'beep boop beer!'
     ht = HaffmanTree()
-    tree = ht.build(s)
-    ht._walk(tree)
-    print(tree)
+    ht.encode(s)
