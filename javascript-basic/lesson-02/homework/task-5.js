802e441d0766994e356b566987597376f9ef761b
KTo <kto@lovetou.ru>
2022-02-04 21:56:47 +0700

    Домашнее задание урок 2

---
+++ javascript-basic/lesson-02/homework/task-5.js
@@ -0,0 +1,19 @@
+/*
+Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+*/
+
+function add(a, b){
+    return a + b
+}
+
+function sub(a, b){
+    return a - b
+}
+
+function mul(a, b){
+    return a * b
+}
+
+function div(a, b){
+    return a / b
+}
