d2d184d5a88021c266341d219f2473a04a6cb516
KTo <kto@lovetou.ru>
2022-09-07 14:46:15 +0700

    Редактирование и удаление поста

--- flask-sobes/flask-prjct/flask_blog/posts/routes.py
+++ flask-sobes/flask-prjct/flask_blog/posts/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask import render_template, Blueprint, request, flash, abort, redirect, url_for
 from flask_blog import db
 from flask_login import login_required, current_user
 from flask_blog.models import Post
@@ -38,3 +38,35 @@ def new_post():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Ваш пост обновлен!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Обновление поста',
+                           form=form, legend='Обновление поста')
+
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Ваш пост был удален!', 'success')
+    return redirect(url_for('posts.all_posts'))
